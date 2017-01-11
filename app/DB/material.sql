2187ba0c2fb62438ec2590922abe089804828d23
Dirc Wang <dirk.wang@samesamechina.com>
2017-01-11 15:06:55 +0800

    update commit

---
+++ app/DB/material.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS `wechat_material`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `wechat_material` (
+  `title` blob NOT NULL,
+  `thumb_media_id` varchar(255) DEFAULT NULL,
+  `show_cover_pic` varchar(400) DEFAULT NULL,
+  `author` varchar(400) DEFAULT NULL,
+  `digest` blob NOT NULL,
+  `content` varchar(400) DEFAULT NULL,
+  `url` varchar(400) DEFAULT NULL,
+  `content_source_url` varchar(400) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `thumb_media_id` (`thumb_media_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
